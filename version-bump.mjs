78ab68cc4885581d4896b47e2fa234d012fb6d4b
Alexei Skachykhin <alexei.skachykhin@live.com>
2024-07-04 16:06:38 -0700

    Version bump no longer conflicts with project formatting

--- version-bump.mjs
+++ version-bump.mjs
@@ -5,8 +5,8 @@ const targetVersion = process.env.npm_package_version;
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+writeFileSync("manifest.json", JSON.stringify(manifest, null, " ".repeat(4)));
 
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+writeFileSync("versions.json", JSON.stringify(versions, null, " ".repeat(4)));
